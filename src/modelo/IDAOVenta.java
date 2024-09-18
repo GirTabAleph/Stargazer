653f3394d2d33630973067649e4fc4cf24b25459
Diego Ortega M. <149025600+GirTabAleph@users.noreply.github.com>
2024-09-17 19:31:51 -0600

    Update IDAOVenta.java
    cancelarVenta ahora lanza IDNotFoundException.

--- src/modelo/IDAOVenta.java
+++ src/modelo/IDAOVenta.java
@@ -1,12 +1,14 @@
 package modelo;
 
+import exceptions.IDNotFoundException;
+
 public interface IDAOVenta {
     
     public boolean insertarVenta(NuevaVenta venta);
     
-    public boolean cancelarVenta(int idVenta);
+    public boolean cancelarVenta(int idVenta) throws IDNotFoundException;
     
-    public boolean buscarVenta(int idVenta);
+    public int buscarVenta(int idVenta);
     
     public NuevaVenta getVenta(int idVenta);
     
