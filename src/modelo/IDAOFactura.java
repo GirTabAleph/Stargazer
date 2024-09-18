cae067dea394c7bfd1f16ffb4741bba9d81839af
Diego Ortega M. <149025600+GirTabAleph@users.noreply.github.com>
2024-09-17 19:24:20 -0600

    Update IDAOFactura.java
    Corrección de la interfaz.

--- src/modelo/IDAOFactura.java
+++ src/modelo/IDAOFactura.java
@@ -1,10 +1,12 @@
 package modelo;
 
+import exceptions.IDNotFoundException;
+
 public interface IDAOFactura {
     
     boolean insertarFactura(Factura factura);
-    Factura obtenerFactura(int numeroFactura);
-    boolean eliminarFactura(int numeroFactura);
-    boolean actualizarFactura(Factura factura);
+    Factura obtenerFactura(Factura factura) throws IDNotFoundException;
+    boolean eliminarFactura(Factura factura) throws IDNotFoundException;
+    boolean actualizarFactura(Factura factura) throws IDNotFoundException;
     
 }
