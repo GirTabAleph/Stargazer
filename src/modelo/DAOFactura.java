f02356a00c2863faf2490d6190a12266fbbe02e3
Diego Ortega M. <149025600+GirTabAleph@users.noreply.github.com>
2024-09-17 19:22:39 -0600

    Update DAOFactura.java
    Corrección de DAOFactura, trabajando la lista de facturas como ArrayList, implementación de los métodos de IDAOFactura

--- src/modelo/DAOFactura.java
+++ src/modelo/DAOFactura.java
@@ -1,29 +1,115 @@
 package modelo;
 
+import exceptions.IDNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DAOFactura implements IDAOFactura {
+    
+    //Atributos.
+    private List facturas = new ArrayList();
 
     @Override
     public boolean insertarFactura(Factura factura) {
+        
+        return facturas.add(factura);
 
     }
-
+    
+    //What the fuck does this do???
     @Override
-    public Factura obtenerFactura(int numeroFactura) {
-
-    }
-
-    @Override
-    public boolean eliminarFactura(int numeroFactura) {
-
-    }
-
-    @Override
-    public boolean actualizarFactura(Factura factura) {
-
+    //Tratar el número de la factura como ID.
+    public Factura obtenerFactura(Factura factura) throws IDNotFoundException{
+        
+        Factura facturaObtenida = null;
+        int posicion = buscarPorNumFactura(factura.getNumeroFactura());
+        
+        if(posicion != -1){
+            
+            facturaObtenida = (Factura)facturas.get(posicion);
+            return facturaObtenida;
+            
+        } else {
+            
+            throw new IDNotFoundException("El número de factura " + 
+                    factura.getNumeroFactura() + " no se encontró.");
+            
         }
-        return false;
+
     }
+
+    @Override
+    public boolean eliminarFactura(Factura factura) throws IDNotFoundException{
+        
+        int posicion = buscarPorNumFactura(factura.getNumeroFactura());
+        
+        if(posicion == -1){
+            
+            throw new IDNotFoundException("El número de factura " 
+                    + factura.getNumeroFactura() + " no existe.");
+            
+        } else {
+            
+           facturas.remove(posicion);
+           return true;
+            
+        }
+
+    }
+
+    @Override
+    //Esta actualización es la modificación.
+    public boolean actualizarFactura(Factura factura) throws IDNotFoundException{
+        
+        int posicion = buscarPorNumFactura(factura.getNumeroFactura());
+        
+        if(posicion == -1){
+            
+            throw new IDNotFoundException("La factura con número " +
+                    factura.getNumeroFactura() + " no existe.");
+            
+            
+        } else {
+            
+            facturas.set(posicion, factura);
+            return true;
+            
+        }
+
+    }
+    
+    public int buscarPorNumFactura(int numFactura){
+        
+        int posicion = 0;
+        boolean encontrado = false;
+        
+        //Buscar.
+        while(posicion <= facturas.size() && !encontrado){
+            
+            Factura factura = (Factura)(facturas.get(posicion));
+            
+            if(factura.getNumeroFactura() == numFactura){
+                
+                encontrado = true;
+            
+            }//Fin if
+            
+            posicion++;
+        
+        }//Fin while       
+        
+        if(encontrado){
+        
+            return posicion;
+            
+        } else {
+            
+            return -1;
+            
+        }
+        
+    }
+    
 }
