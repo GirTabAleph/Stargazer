cf16adf876256d44dbaf20c326cfe42354314ead
GirTabAleph <nineeencycles@gmail.com>
2024-09-21 15:37:36 -0600

    Actualización de la interfaz IUProducto para incluir los nuevos métodos.

--- src/vista/IUProducto.java
+++ src/vista/IUProducto.java
@@ -9,9 +9,11 @@ import modelo.Producto;
 public interface IUProducto {
     
     public Producto leerProducto();
-    public void mostrarProducto();
-    public int leerIdProducto();
-    public String leerNombreProducto();
-    public void mostrarListaProductos();
+    public void mostrarProducto(Producto producto);
+    public int leerIdProducto(String texto);
+    public String leerNombreProducto(String texto);
+    public void mostrarListaProductos(Producto[] productos);
+    public int menuProductos();
+    public void mensaje(String cadena);
     
 }
