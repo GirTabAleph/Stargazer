c06264cfbaad015871090997e2d3d745597636bc
Issrael <issrael808@gmail.com>
2024-10-04 11:45:00 -0600

    ControlIGUProductos.java

--- src/controlador/ControlIGUProductos.java
+++ src/controlador/ControlIGUProductos.java
@@ -1,11 +1,15 @@
 package controlador;
 
+import exceptions.ProductIDNotFoundException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import modelo.DAOProductoArrayList;
 import modelo.IDAOProducto;
+import modelo.Producto;
 import vista.IGUProductos;
 import vista.IUTextoProducto;
 
@@ -29,19 +33,99 @@ public class ControlIGUProductos implements ActionListener{
         
         String comando = e.getActionCommand();
         
-        switch(comando){
+        try {
             
-            case "Aceptar":
-                productos.agregarProducto(igu.getProducto());
-                IUTextoProducto texto = new IUTextoProducto(); 
-                texto.mostrarListaProductos((List)productos);
-                break;
-                
-            case "Cancelar":
-            
-            case "Limpiar":
+            switch(comando){
+
+
+                case "Aceptar":
+                    productos.agregarProducto(igu.getProducto(true));
+                    IUTextoProducto texto = new IUTextoProducto(); 
+                    texto.mostrarListaProductos(productos.getAllProductos());
+                    break;
+
+                case "Eliminar":
+
+                    productos.borrarProducto(igu.getId());
+
+                    IUTextoProducto texto1 = new IUTextoProducto(); 
+                    texto1.mostrarListaProductos(productos.getAllProductos());
+                    break;    
+
+                case "Cancelar":
+                    igu.dispose();
+                    break;
+
+                case "Limpiar campos":
+                    igu.limpiarCampos();
+                    break;
+
+                case "Nuevo":
+                    nuevo();
+                    break;
+
+                case "Borrar":
+                    borrar();
+                    break;
+
+                case "Modificar":
+                    modificar();
+                    break;
+
+                case "Buscar":                   
+                    buscar();
+                    break;
+                    
+                case "Editar":
+                    editar();
+                    break;
+
+            }
+        
+        } catch (ProductIDNotFoundException ex) {
+        
+            System.out.println(ex.getMessage());
         }
         
     }
     
+    public void nuevo(){
+    
+        igu.desactivarID();
+        igu.activarCampos();
+        igu.cambiarAccion("Aceptar");
+        
+    }
+    
+    public void borrar(){
+        
+        igu.activarID();
+        igu.desactivarCampos();
+        igu.cambiarAccion("Eliminar");
+    }
+    
+    public void editar(){
+        
+        igu.activarID();
+        igu.desactivarCampos();
+        igu.cambiarAccion("Buscar");   
+    }
+    
+    public void buscar() throws ProductIDNotFoundException{
+        
+        Producto producto = productos.getProductoById(igu.getId());
+        
+        igu.setProducto(producto);
+        igu.cambiarAccion("Modificar");
+        
+        igu.activarCampos();
+        igu.desactivarID();
+    }
+    public void modificar() throws ProductIDNotFoundException{
+        
+        productos.modificarProducto(igu.getId(), igu.getProducto(false));
+        
+        IUTextoProducto texto1 = new IUTextoProducto(); 
+        texto1.mostrarListaProductos(productos.getAllProductos());
+    }
 }
