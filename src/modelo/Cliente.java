fdbdbb04d6739241fdbd5ce369c99508de615b8b
Diego Ortega M. <149025600+GirTabAleph@users.noreply.github.com>
2024-11-03 10:05:16 -0600

    Implementación bajo factory method.

--- src/modelo/Cliente.java
+++ src/modelo/Cliente.java
@@ -1,45 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
 import javax.swing.JOptionPane;
 import exceptions.InvalidDateFormatException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Cliente extends Persona {
     
-    private String fechaPrimeraCompra;
-    private int idCliente;
+    private Date fechaPrimeraCompra;
     
     
     // Constructor
 
-    public Cliente(String nombre, String rfc, String domicilio, String telefono, String zona) {
-        super(nombre, rfc, domicilio, telefono, zona);
+    public Cliente(int idPersona, String nombre, String rfc, String domicilio, String telefono, int idZona) {
+        super(idPersona, nombre, rfc, domicilio, telefono, idZona);
     }
     
     
-    public Cliente(String nombre, String rfc, String domicilio, String telefono,
-            String zona, String fechaPrimeraCompra, int idCliente) {
+    public Cliente(int idPersona, String nombre, String rfc, String domicilio, String telefono,
+            int idZona, Date fechaPrimeraCompra) {
         
-        super(nombre, rfc, domicilio, telefono, zona);
+        super(idPersona, nombre, rfc, domicilio, telefono, idZona);
         
         this.fechaPrimeraCompra = fechaPrimeraCompra;
-        this.idCliente = idCliente;
+        
     }
 
     // Getters y setters
-    public String getFechaPrimeraCompra() {
+    public Date getFechaPrimeraCompra() {
         return fechaPrimeraCompra;
     }
     
-    public int getIdCliente(){
-        return idCliente;
-    }
-    
+        
     /*
      public void setFechaPrimeraCompra(String fechaPrimeraCompra) {
         this.fechaPrimeraCompra = fechaPrimeraCompra;
@@ -49,43 +42,27 @@ public class Cliente extends Persona {
     //Ponce, perdóneme por la aberración que va a leer.
     //-Atte. Diego Ortega.
     
-    public void setFechaPrimeraCompra(String fechaPrimeraCompra) throws InvalidDateFormatException{		
+    public void setFechaPrimeraCompra(Date fechaPrimeraCompra){
         
-        //Este sdf es para validar el formato de fecha.
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MMM/dd");
-        sdf.setLenient(false);
-        
-        //Intentar parsing de la fecha, si es válida se asigna.
-        try{
+       
+        this.fechaPrimeraCompra = fechaPrimeraCompra;
             
-            sdf.parse(fechaPrimeraCompra);
-            this.fechaPrimeraCompra = fechaPrimeraCompra;
-            
-        }catch (ParseException ex){
-            
-            //Cuando falle el parsing, tirar la excepción de fecha.
-            throw new InvalidDateFormatException("Manín, el formato de fecha está mal.");
-        }
-        
     
     }
     
-    
-    public void setIdCliente(int idCliente){
-        this.idCliente = idCliente;
-    }
     
     // Método para capturar datos del cliente
     public static Cliente crearCliente() {
+        
+        int idPersona = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID del cliente: "));
         String nombre = JOptionPane.showInputDialog("Ingrese el nombre del cliente:");
         String rfc = JOptionPane.showInputDialog("Ingrese el RFC del cliente:");
         String domicilio = JOptionPane.showInputDialog("Ingrese el domicilio del cliente:");
         String telefono = JOptionPane.showInputDialog("Ingrese el teléfono del cliente:");
-        String zona = JOptionPane.showInputDialog("Ingrese la zona del cliente:");
-        String fechaPrimeraCompra = JOptionPane.showInputDialog("Ingrese la fecha de la primera compra del cliente:");
-        int idCliente = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el identificador (ID) del cliente:"));
+        int idZona = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID de la zona: "));
+        Date fechaPrimeraCompra = null;//JOptionPane.showInputDialog("Ingrese la fecha de la primera compra del cliente:");
 
-        return new Cliente(nombre, rfc, domicilio, telefono, zona, fechaPrimeraCompra, idCliente);
+        return new Cliente(idPersona, nombre, rfc, domicilio, telefono, idZona, fechaPrimeraCompra);
     }
 
 }
