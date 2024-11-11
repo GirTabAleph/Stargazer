f3b189ac9408f063ccfc9570e3417886f9e27b4d
Diego Ortega M. <149025600+GirTabAleph@users.noreply.github.com>
2024-11-11 14:13:15 -0600

    Create ConexionBD.java

---
+++ src/modelo/bd/ConexionBD.java
@@ -0,0 +1,50 @@
+package modelo.bd;
+
+import java.sql.*;
+/**
+ *
+ * @author Diego Ortega
+ */
+public class ConexionBD {
+    
+    //Esto trabajará como singleton.
+    private static Connection conexion = null;
+    
+    private final String url = "jdbc:mysql://localhost:3306/ventas";
+    private final String usuario = "root";
+    private final String contrasenia = "Dortega1008.";
+    
+    private ConexionBD(){
+        
+        //Las conexiones deben estár protegidas.
+        try{
+            
+            conexion = DriverManager.getConnection(url, usuario, contrasenia);
+            
+        } catch(SQLException sqlex){
+            
+            sqlex.getStackTrace();
+            
+        }
+        
+    }
+    
+    public static Connection getInstance(){
+        
+        if(conexion == null){
+            
+            new ConexionBD();
+            
+        }
+        
+        return conexion;
+        
+    }
+    
+    public static void main(String args[]){
+        
+        Connection conexion = ConexionBD.getInstance();
+        
+    }
+    
+}
