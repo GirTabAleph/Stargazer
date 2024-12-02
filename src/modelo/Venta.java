101802c308b5cd8d12a157d461417c70424485cc
Diego Ortega M. <149025600+GirTabAleph@users.noreply.github.com>
2024-12-02 07:49:02 -0600

    Update Venta.java

--- src/modelo/Venta.java
+++ src/modelo/Venta.java
@@ -7,6 +7,8 @@ TO-DO: Cambiar el producto por un int, no queremos el objeto entero, solo el ID.
 package modelo;
 
 import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 
 //Jesus Fucking Christ, who wrote this madness?
 //Nevermind, i did.
@@ -19,56 +21,39 @@ public class Venta /*implements IVenta*/{
     private static int seqId = 0;
     
     private int idVenta;
-    private ProductoVendido productoV; //¿?
-    private int idProductoV;
-    private double costoUnitario; 
-    private double costoTotal;
+    //Total ventas será método.
     private Date fechaVenta;
     private int idZona;
     private int idCliente;
     private int idVendedor;
     private boolean requiereFactura;
-
-    public Venta(int idProductoV, double costoUnitario, double costoTotal, 
-            Date fechaVenta, int idZona, int idCliente, int idVendedor,
-            boolean requiereFactura) {
-        
-        this.idProductoV = idProductoV;
-        this.costoUnitario = costoUnitario;
-        this.costoTotal = costoTotal;
-        this.fechaVenta = fechaVenta;
-        this.idZona = idZona;
-        this.idCliente = idCliente;
-        this.idVendedor = idVendedor;
-        this.requiereFactura = requiereFactura;
-   
-    }
-
-    public Venta(int idVenta, ProductoVendido productoV, int idProductoV, double costoUnitario, double costoTotal, Date fechaVenta, int idZona, int idCliente, int idVendedor, boolean requiereFactura) {
-        
-        if(idVenta == 0){
-            
-            this.idVenta = idVenta;
-            
-        } else {
-            
-            this.idVenta = incrementarId();
-            
-        }
-        
-        this.idVenta = idVenta;
-        this.productoV = productoV;
-        this.idProductoV = idProductoV;
-        this.costoUnitario = costoUnitario;
-        this.costoTotal = costoTotal;
-        this.fechaVenta = fechaVenta;
-        this.idZona = idZona;
-        this.idCliente = idCliente;
-        this.idVendedor = idVendedor;
-        this.requiereFactura = requiereFactura;
-   
-    }
     
+    private List <ProductoVendido> productosV  = new ArrayList<ProductoVendido>(); //Todos los prods. de la venta.
+
+    public Venta(int idVenta, Date fechaVenta, int idZona, int idCliente, int idVendedor, boolean requiereFactura, List<ProductoVendido> productosV) {
+    
+        this.idVenta = idVenta;
+        this.fechaVenta = fechaVenta;
+        this.idZona = idZona;
+        this.idCliente = idCliente;
+        this.idVendedor = idVendedor;
+        this.requiereFactura = requiereFactura;
+        this.productosV = productosV;
+    
+    }
+
+    public Venta(Date fechaVenta, int idZona, int idCliente, int idVendedor, boolean requiereFactura, List<ProductoVendido> productosV) {
+    
+        this.fechaVenta = fechaVenta;
+        this.idZona = idZona;
+        this.idCliente = idCliente;
+        this.idVendedor = idVendedor;
+        this.requiereFactura = requiereFactura;
+        this.productosV = productosV;
+    
+    }
+
+    /*
     public Venta crearVenta(int idProductoV, double cantidad, Date fechaVenta, 
                                    int idZona, int idCliente, int idVendedor, boolean requiereFactura) {
 
@@ -78,55 +63,100 @@ public class Venta /*implements IVenta*/{
 
         return new Venta(idProductoV, costoUnitario, costoTotal, fechaVenta, idZona, idCliente, idVendedor, requiereFactura);
     }
+    */
     
-    public boolean requiereFactura() {
-        return requiereFactura;
-    }
     
-    public int getIdCliente() {
-        return idCliente;
-    }
-    
-    public Date getFechaVenta() {
-        return fechaVenta;
-    }
-    
-    public ProductoVendido getProductoVendido() {
-        return productoV;
-    }
-    
-    public double getCostoTotal() {
-        return costoTotal;
-    }
-
-    public ProductoVendido getProductoV() {
-        return productoV;
-    }
-
-    public int getIdProductoV() {
-        return idProductoV;
-    }
-
-    public double getCostoUnitario() {
-        return costoUnitario;
-    }
-
-    public int getIdZona() {
-        return idZona;
-    }
-
-    public int getIdVendedor() {
-        return idVendedor;
-    }
-
-    public boolean getRequiereFactura() {
-        return requiereFactura;
-    }
     
     private static int incrementarId(){
         
         return ++seqId;
         
     } 
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Date getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(Date fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public int getIdZona() {
+        return idZona;
+    }
+
+    public void setIdZona(int idZona) {
+        this.idZona = idZona;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public boolean getRequiereFactura() {
+        return requiereFactura;
+    }
+
+    public void setRequiereFactura(boolean requiereFactura) {
+        this.requiereFactura = requiereFactura;
+    }
+
+    public List <ProductoVendido> getProductosV() { return productosV;}
+
+    public void setProductosV(List <ProductoVendido> productosV) {
+        this.productosV = productosV;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + this.idVenta;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Venta other = (Venta) obj;
+        return this.idVenta == other.idVenta;
+    }
+
+    @Override
+    public String toString() { //Dar formato, un dato por renglón y con \n.
+        return "Venta{" + "idVenta=" + idVenta + ", fechaVenta=" +
+                fechaVenta + ", idZona=" + idZona + ", idCliente=" +
+                idCliente + ", idVendedor=" + idVendedor + ", requiereFactura=" + 
+                requiereFactura + ", productosV=" + productosV + '}';
+    }
+    
+    
     
 }
