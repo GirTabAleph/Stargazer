f56e06212a93072c2e52997e2d7aa2d7c060d904
Issrael <issrael808@gmail.com>
2024-12-02 20:57:16 -0600

    Update DAOVentaBD.java

--- src/modelo/bd/DAOVentaBD.java
+++ src/modelo/bd/DAOVentaBD.java
@@ -1,11 +1,12 @@
-//Nota: Hacerle su adapter a la interfaz para darle versatilidad.
-
 package modelo.bd;
 
 import exceptions.IDNotFoundException;
+import exceptions.ProductIDNotFoundException;
 import java.sql.*;
 import java.util.*;
 import modelo.IDAOVenta;
+import modelo.NuevaVenta;
+import modelo.Producto;
 import modelo.ProductoVendido;
 import modelo.Venta;
 
@@ -41,7 +42,7 @@ public class DAOVentaBD implements IDAOVenta{
                                     	idZona,
                                     	idCliente,
                                     	idVendedor,
-                                    	requiereFactura,
+                                    	requiereFactura
                                     
                                     ) VALUES (
                                     	
@@ -71,13 +72,18 @@ public class DAOVentaBD implements IDAOVenta{
             
             if(instruccionPrepVenta.executeUpdate() == 1){
                 
+                
                 resultadoVenta = instruccionPrepVenta.getGeneratedKeys();
                 
+            
                 if(resultadoVenta.next()){
                     
                     idVenta = resultadoVenta.getInt(1);
+                    
+                    
+                    
                     List<ProductoVendido> listaProductos = venta.getProductosV();
-        
+                    
                     String insercionProducto = """
                                    INSERT INTO productoVendido (
                                    
@@ -104,13 +110,7 @@ public class DAOVentaBD implements IDAOVenta{
 
                         for(ProductoVendido producto: listaProductos){
 
-                            /*
-                            idVenta,
-                                                    idProducto,
-                                                    cantidad,
-                                                    precio,
-                                                    descuento
-                            */
+                            
 
                             instruccionPrepProducto.setInt(1, idVenta); //Variable generada
                             instruccionPrepProducto.setInt(2, producto.getIdProducto());
@@ -118,22 +118,27 @@ public class DAOVentaBD implements IDAOVenta{
                             instruccionPrepProducto.setDouble(4, producto.getPrecio());
                             instruccionPrepProducto.setDouble(5, producto.getDescuento());
 
-                        } //For each
+                        } //For each 
 
+                        if(instruccionPrepProducto.executeUpdate() == 1){
+                        
+                            conexion.commit();
+                            
+                
+                        } else {
 
+                            conexion.rollback();
+                            
+                            correcto = false;
+                        }
+                    }catch(SQLException e2){
+                        
+                        System.out.println("No se guardo");
                     }
-            
                 }
-
-                //ESTO ES LO ÚLTIMO A HACER LPM.
-                conexion.commit();
-
-                
-            } else {
-                
-                conexion.rollback();
-                
+            
             }
+                
             
  
         } catch(SQLException sqlex){
@@ -144,47 +149,7 @@ public class DAOVentaBD implements IDAOVenta{
         
         
         
-        //Try con recursos.
-        try(PreparedStatement instruccionPreparada 
-                = conexion.prepareStatement(insercionVenta)){
-            
-            conexion.setAutoCommit(false);
-           
-            instruccionPreparada.setDate(1, new java.sql.Date(venta.getFechaVenta().getTime()));
-            instruccionPreparada.setInt(2, venta.getIdZona());
-            instruccionPreparada.setInt(3, venta.getIdCliente());
-            instruccionPreparada.setInt(4, venta.getIdVendedor());
-            instruccionPreparada.setBoolean(5, venta.getRequiereFactura());
-            
-            /*
-            
-            */
-            
-            if(instruccionPreparada.executeUpdate() == 1){
-                
-                conexion.commit();
-                //System.out.println("Insertado");
-                
-            } else {
-                
-                conexion.rollback();
-                correcto = false;
-                
-            }
-           
-            
-        } catch(SQLIntegrityConstraintViolationException sqlicve){
-            
-            System.out.println("Error: ID no es único.");
-            correcto = false;
-            
-        } catch(SQLException sqlex){
-            
-            System.out.println("Error de SQL, sabrá dios que pasó.");
-            sqlex.printStackTrace();
-            correcto = false;
-            
-        }
+        
         
         return correcto;
         
@@ -196,8 +161,10 @@ public class DAOVentaBD implements IDAOVenta{
         
         boolean correcto = true;
         String instruccion = """
-                             DELETE FROM venta 
-                             	WHERE id = ?
+                             UPDATE venta 
+                             SET cancelada = true 
+                             	WHERE idVenta = ?
+                             
                              """;
         try(PreparedStatement instruccionPreparada
                 = conexion.prepareStatement(instruccion)){
@@ -228,16 +195,122 @@ public class DAOVentaBD implements IDAOVenta{
         
     }
 
-    @Override
-    public int buscarVenta(int idVenta) { //Regresa posición, esto es 
-        // en arraylist. No lo usaremos en BD. Cuando cree el adapter algún día 
-        // esto ya no estará aquí.
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
+    
 
     @Override
     public Venta getVenta(int idVenta) { //Regresa el objeto
-        throw new UnsupportedOperationException("Not supported yet."); 
+        String instruccion = """
+                             SELECT * 
+                             FROM Venta
+                             WHERE idVenta = ?
+                             """
+                             ;
+        
+        Venta venta = null;
+        
+        try(PreparedStatement instruccionPreparada = 
+                conexion.prepareStatement(instruccion)){
+            
+            //Sin commit, no se modifica nada.
+            instruccionPreparada.setInt(1, idVenta); //1era interrogante, id.
+            
+            try(ResultSet conjuntoResultados = instruccionPreparada.executeQuery()){
+                
+                if(conjuntoResultados.next()){
+                    //(int idVenta, Date fechaVenta, int idZona, int idCliente, int idVendedor, boolean requiereFactura, 
+                    //List<ProductoVendido> productosV) tabla producto vendido, se extrae del mismo id, un prepare statetmen al mismo
+                    //como son varios renglones se hace lo de get alll why next y extraer cada tupla 
+                    //crear una lista insertar en la lista, insertar en la lista y crear la venta con la lista
+                    venta = new Venta(
+                        conjuntoResultados.getInt("idVenta"),
+                        conjuntoResultados.getDate("fechaVenta"),
+                        conjuntoResultados.getInt("idZona"),
+                        conjuntoResultados.getInt("idCliente"),
+                        conjuntoResultados.getInt("idVendedor"),
+                        conjuntoResultados.getBoolean("requiereFactura"),
+                        conjuntoResultados.getBoolean("cancelada"));//modificar venta
+                    
+                } else {
+                    
+                    throw new ProductIDNotFoundException(id);
+                    
+                }
+                
+            } catch (SQLException sqlex){
+                
+                sqlex.printStackTrace();
+                
+            }
+            
+            
+        } catch(SQLException sqlex){
+            
+            sqlex.printStackTrace();
+            
+        }
+        
+        
+        
+        return venta;
     }
     
+    
+    public static void main(String [] args)throws IDNotFoundException{
+        
+        DAOVentaBD dao = new DAOVentaBD();
+        
+        List<ProductoVendido> lista = new ArrayList<>(); 
+        lista.add(new ProductoVendido(1,1.0, 1, 1.0));
+        
+        if(dao.insertarVenta(new Venta(1,new java.util.Date(), 1,
+                1,1,false, lista))){
+            
+            System.out.println("Ya inserte");
+        }else{
+            
+            System.out.println("error");
+        }
+        
+        if(dao.cancelarVenta(1)){
+            
+            System.out.println("Cancelado");
+        }else{
+            
+            System.out.println("Error");
+        }
+    }
+ 
+    
+    
 }
+/*
+drop table productovendido cascade;
+drop table venta;
+
+ CREATE TABLE venta (
+    
+    idVenta INT PRIMARY KEY AUTO_INCREMENT,
+    fechaVenta DATETIME NOT NULL,
+    idZona INT NOT NULL,
+    idCliente INT NOT NULL,
+    idVendedor INT NOT NULL,
+    requiereFactura BOOLEAN NOT NULL,
+    cancelada BOOLEAN 
+
+    );
+
+CREATE TABLE productoVendido (
+
+	idVenta INT NOT NULL, 
+	idProducto INT NOT NULL REFERENCES venta(idProducto),
+	cantidad INT NOT NULL,
+	precio DECIMAL(5.2) NOT NULL,
+	descuento decimal (5.2) NULL,
+
+	PRIMARY KEY (idVenta, idProducto),
+	FOREIGN KEY (idVenta) REFERENCES venta(idVenta)
+
+);
+
+
+*/
