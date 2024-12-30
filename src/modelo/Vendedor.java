ecd491fe5f5dd7d65981cebf17c1a483c7650c75
Diego Ortega M. <149025600+GirTabAleph@users.noreply.github.com>
2024-12-29 19:38:49 -0600

    Update Vendedor.java
    Modificaciones a Vendedor

--- src/modelo/Vendedor.java
+++ src/modelo/Vendedor.java
@@ -1,76 +1,85 @@
-package modelo;
+//Modificado el 29 de diciembre de 2024.
 
+package modelo;
 
 import java.util.Date;
 import javax.swing.JOptionPane;
 
-//Esta NO ES DTO. Tiene un método para crear vendedores
-//y eso hace que ya no sea DTO.
+//Pues me cago en Dios, ya se volvió DTO.
 
 public class Vendedor extends Persona {
+    
+    private int idVendedor;
     private Date fechaContratacion;
     private double porcentajeComisiones;
-    private int idVendedor;
 
     // Constructor
-
-    public Vendedor(String nombre, String rfc, String domicilio, String telefono, Zona zona) {
+    public Vendedor(int idPersona, String nombre, String rfc, String domicilio, String telefono, int idZona) {
         
-        super(nombre, rfc, domicilio, telefono, zona);
+        super(idPersona, nombre, rfc, domicilio, telefono, idZona);
 
     }
     
     
-    public Vendedor(String nombre, String rfc, String domicilio, String telefono, 
-            Zona zona, Date fechaContratacion, double porcentajeComisiones, 
-            int idVendedor) {
+    public Vendedor(int idPersona, String nombre, String rfc, String domicilio, String telefono, 
+            int idZona, Date fechaContratacion, double porcentajeComisiones, int idVendedor) {
         
-        super(nombre, rfc, domicilio, telefono, zona);
+        super(idPersona, nombre, rfc, domicilio, telefono, idZona);
         
         this.fechaContratacion = fechaContratacion;
         this.porcentajeComisiones = porcentajeComisiones;
         this.idVendedor = idVendedor;
+        
     }
 
-    //Setters
+    //Setters y getters.
+
+    public int getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(int idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public Date getFechaContratacion() {
+        return fechaContratacion;
+    }
 
     public void setFechaContratacion(Date fechaContratacion) {
         this.fechaContratacion = fechaContratacion;
+    }
+
+    public double getPorcentajeComisiones() {
+        return porcentajeComisiones;
     }
 
     public void setPorcentajeComisiones(double porcentajeComisiones) {
         this.porcentajeComisiones = porcentajeComisiones;
     }
     
-    public void setIdVendedor(int idVendedor){
-        this.idVendedor = idVendedor;
-    }
-
-
-    public Date getFechaContratacion() {
-        return fechaContratacion;
-    }
-
-    public double getPorcentajeComisiones() {
-        return porcentajeComisiones;
-    }
-    
-    public int getIdVendedor(){
-        return idVendedor;
-    }
-
-    // Método para capturar datos del vendedor
+    //No sirve, ignorar.
+    /*
     public static Vendedor crearVendedor() {
+        
+        int idPersona = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el identificador (ID) del vendedor:"));
         String nombre = JOptionPane.showInputDialog("Ingrese el nombre del vendedor:");
         String rfc = JOptionPane.showInputDialog("Ingrese el RFC del vendedor:");
         String domicilio = JOptionPane.showInputDialog("Ingrese el domicilio del vendedor:");
         String telefono = JOptionPane.showInputDialog("Ingrese el teléfono del vendedor:");
-        Zona zona = null; //JOptionPane.showInputDialog("Ingrese la zona de entrega del vendedor:");
-        Date fechaContratacion = null; //JOptionPane.showInputDialog("Ingrese la fecha de contratación del vendedor:");
+        int idZona = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ID de la zona:"));
+        Date fechaContratacion = null;//JOptionPane.showInputDialog("Ingrese la fecha de contratación del vendedor:");
         double porcentajeComisiones = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el porcentaje de comisiones del vendedor:"));
-        int idVendedor = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el identificador (ID) del vendedor:"));
+        
 
-        return new Vendedor(nombre, rfc, domicilio, telefono, zona, fechaContratacion, porcentajeComisiones, idVendedor);
+        return new Vendedor(idPersona, nombre, rfc, domicilio, telefono, idZona, fechaContratacion, porcentajeComisiones);
     }
-}
+    */
 
+    @Override
+    public String toString() {
+        return super.toString() + "Vendedor{" + "fechaContratacion=" + fechaContratacion + ", porcentajeComisiones=" + porcentajeComisiones + '}';
+    }
+    
+    
+}
